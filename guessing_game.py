097d1c3d72a6e36dd8325b3e116af8be2189e281
Rachit <rachitp6@gmail.com>
2024-04-22 22:12:14 +0530

    second commit

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,14 @@
 import math
 import random
+# import libraries
+# store lower and upper limit in bariable
 lower = int(input('lower bound '))
 upper = int(input('upper bound '))
+
+# store random number in x
 x = random.randint(lower,upper)
+
+# limit the chance to guess
 print('\n\tyou have',math.log(upper-lower + 1,2),'chance to guess the number\n')
 count = 0
 while count < math.log(upper-lower + 1,2) :
@@ -16,6 +22,7 @@ while count < math.log(upper-lower + 1,2) :
         print('your no. is too small')
     count += 1
 
+# if max chance gate over 
 if count >= math.log(upper - lower + 1, 2):
     print("\nThe number is %d" % x)
     print("\tBetter Luck Next time!")           
